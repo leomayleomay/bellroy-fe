ead2d1093228b05fa3581a83b1d8c245c2d96641
Hao <hao.liu@daisee.com>
2025-04-28 11:14:02 +1000

    Introduce elm-review

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 Nice to have:
 ---
 * [elm-ui](https://package.elm-lang.org/packages/mdgriffith/elm-ui/latest/)
-* [elm-review](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/)
--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    yarn
     nodejs
     elmPackages.elm
+    elmPackages.elm-live
     elmPackages.elm-format
+    elmPackages.elm-review
   ];
 }
